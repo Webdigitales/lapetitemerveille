e2b6444f5f05c9ea7abbd9a8155cb16a490a399d
Kevin Marion <marionkevin91@gmail.com>
2020-06-04 17:25:18 +0200

    debug map js

--- web/assets/js/map.js
+++ web/assets/js/map.js
@@ -3,128 +3,131 @@
   // The maximum width of the info window is set to 200 pixels.
 
   function initMap() {
-    
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 12,
-      center: destination,
-	  styles: [
-    {
-        "featureType": "road",
-        "stylers": [
-            {
-                "hue": "#5e00ff"
-            },
-            {
-                "saturation": -79
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "stylers": [
-            {
-                "saturation": -78
-            },
-            {
-                "hue": "#6600ff"
-            },
-            {
-                "lightness": -47
-            },
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "road.local",
-        "stylers": [
-            {
-                "lightness": 22
-            }
-        ]
-    },
-    {
-        "featureType": "landscape",
-        "stylers": [
-            {
-                "hue": "#6600ff"
-            },
-            {
-                "saturation": -11
-            }
-        ]
-    },
-    {},
-    {},
-    {
-        "featureType": "water",
-        "stylers": [
-            {
-                "saturation": -65
-            },
-            {
-                "hue": "#1900ff"
-            },
-            {
-                "lightness": 8
-            }
-        ]
-    },
-    {
-        "featureType": "road.local",
-        "stylers": [
-            {
-                "weight": 1.3
-            },
-            {
-                "lightness": 30
-            }
-        ]
-    },
-    {
-        "featureType": "transit",
-        "stylers": [
-            {
-                "visibility": "simplified"
-            },
-            {
-                "hue": "#5e00ff"
-            },
-            {
-                "saturation": -16
-            }
-        ]
-    },
-    {
-        "featureType": "transit.line",
-        "stylers": [
-            {
-                "saturation": -72
-            }
-        ]
-    },
-    {}
-]
 
-    });
+    if (document.getElementById('map').length > 0) {
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 12,
+            center: destination,
+            styles: [
+                {
+                    "featureType": "road",
+                    "stylers": [
+                        {
+                            "hue": "#5e00ff"
+                        },
+                        {
+                            "saturation": -79
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi",
+                    "stylers": [
+                        {
+                            "saturation": -78
+                        },
+                        {
+                            "hue": "#6600ff"
+                        },
+                        {
+                            "lightness": -47
+                        },
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.local",
+                    "stylers": [
+                        {
+                            "lightness": 22
+                        }
+                    ]
+                },
+                {
+                    "featureType": "landscape",
+                    "stylers": [
+                        {
+                            "hue": "#6600ff"
+                        },
+                        {
+                            "saturation": -11
+                        }
+                    ]
+                },
+                {},
+                {},
+                {
+                    "featureType": "water",
+                    "stylers": [
+                        {
+                            "saturation": -65
+                        },
+                        {
+                            "hue": "#1900ff"
+                        },
+                        {
+                            "lightness": 8
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.local",
+                    "stylers": [
+                        {
+                            "weight": 1.3
+                        },
+                        {
+                            "lightness": 30
+                        }
+                    ]
+                },
+                {
+                    "featureType": "transit",
+                    "stylers": [
+                        {
+                            "visibility": "simplified"
+                        },
+                        {
+                            "hue": "#5e00ff"
+                        },
+                        {
+                            "saturation": -16
+                        }
+                    ]
+                },
+                {
+                    "featureType": "transit.line",
+                    "stylers": [
+                        {
+                            "saturation": -72
+                        }
+                    ]
+                },
+                {}
+            ]
 
-    
-    var infowindow = new google.maps.InfoWindow({
-      content: contentString,
-      maxWidth: 200
-    });
+        });
 
-    var marker = new google.maps.Marker({
-      position: destination,
-      map: map,
- 
-      icon: {
-		url: "https://images.vexels.com/media/users/3/131261/isolated/lists/b2e48580147ca0ed3f970f30bf8bb009-map-location-marker.png",
-		scaledSize: new google.maps.Size(64, 64)
-	}
-    });
-    marker.addListener('click', function() {
-      infowindow.open(map, marker);
-    });
+
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString,
+            maxWidth: 200
+        });
+
+        var marker = new google.maps.Marker({
+            position: destination,
+            map: map,
+
+            icon: {
+                url: "https://images.vexels.com/media/users/3/131261/isolated/lists/b2e48580147ca0ed3f970f30bf8bb009-map-location-marker.png",
+                scaledSize: new google.maps.Size(64, 64)
+            }
+        });
+        marker.addListener('click', function() {
+            infowindow.open(map, marker);
+        });
+    }
+
   }
